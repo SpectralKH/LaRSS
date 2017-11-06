715944492571a3144fcf0d09dafe22d4d4ea4bbd
KH <kasperkh.kh@gmail.com>
2017-11-06 18:12:50 +0100

    Gist creation and loading

--- docs/index.js
+++ docs/index.js
@@ -69,6 +69,7 @@ window.xhr = function(req, options) {
 })();
 
 var pat = localStorage.getItem("pat");
+var gistId = localStorage.getItem("gistId");
 function getFilename(pat) {
     return {
         json: "larss-"+pat.slice(0,4)+".json",
@@ -76,10 +77,78 @@ function getFilename(pat) {
     };
 }
 
-function loadFeed(callback) {
+load(pat);
+function load(pat, callback) {
     if (pat) {
-
+        // if gistId is already saved, load it. Otherwise, find the gistId
+        if (gistId) {
+            findGist(pat, gistId, function(content) {
+                // if the gist was found, load it. Otherwise
+                if (content) {
+                    gistFound(content);
+                    if (callback) callback();
+                } else {
+                    createGist(pat, function(id, content) {
+                        localStorage.setItem("gistId", id);
+                        gistId = id;
+                        gistFound(content);
+                        if (callback) callback();
+                    });
+                    if (callback) callback();
+                }
+            });
+        } else {
+            findGistId(pat, function(id) {
+                if (id) {
+                    findGist(pat, id, function(content) {
+                        gistFound(content);
+                        if (callback) callback();
+                        resizeTextareas();
+                    });
+                } else {
+                    createGist(pat, function(id, content) {
+                        localStorage.setItem("gistId", id);
+                        gistId = id;
+                        gistFound(content);
+                        if (callback) callback();
+                    });
+                }
+            });
+        }
     }
+}
+function gistFound(json, updatedAt) {
+    var container = document.querySelector(".container");
+    container.innerHTML = '<img src="logo.png" class="logo">';
+    var generalInfo = document.querySelector(".container-sample .general-info").cloneNode(true);
+    var gio = json.generalInfo; // generalInfoObject
+    var io = json.items; // generalInfoObject
+    container.appendChild(generalInfo);
+    generalInfo.querySelector("input.title").value = gio.title;
+    generalInfo.querySelector("input.author").value = gio.author;
+    generalInfo.querySelector("input.subtitle").value = gio.subtitle;
+    generalInfo.querySelector("textarea.description").value = gio.description;
+    generalInfo.querySelector("input.url").value = gio.url;
+    generalInfo.querySelector("input.imageURL").value = gio.imageURL;
+    generalInfo.querySelector('select.language').value = gio.language;
+    generalInfo.querySelector('select.category').value = gio.category;
+    generalInfo.querySelector('select.explicit').value = gio.explicit;
+    generalInfo.querySelector("input.copyright").value = gio.copyright;
+    generalInfo.querySelector("input.name").value = gio.name;
+    generalInfo.querySelector("input.email").value = gio.email;
+    for (var i = 0; i < json.items.length; i++) {
+        var itemSample = document.querySelector(".container-sample .item").cloneNode(true);
+        container.appendChild(itemSample);
+        itemSample.querySelector("input.guid").value = io[i].guid;
+        itemSample.querySelector("input.title").value = io[i].title;
+        itemSample.querySelector("input.audioURL").value = io[i].audioURL;
+        itemSample.querySelector("input.imageURL").value = io[i].imageURL;
+        itemSample.querySelector('select.explicit').value = io[i].explicit;
+        itemSample.querySelector("input.date").value = io[i].date;
+        itemSample.querySelector("input.duration").value = io[i].duration;
+        itemSample.querySelector("textarea.summary").value = io[i].summary;
+    }
+    resizeTextareas();
 }
 
 (function patDialog() {
@@ -94,15 +163,13 @@ function loadFeed(callback) {
     saveButton.addEventListener("click", function() {
         console.log("saving access token");
         // save personal access token
-        pat = pat.value;
+        pat = patInput.value;
         localStorage.setItem("pat", pat);
         jsonGistId = null;
         rssGistId = null;
-        gistDate = null;
         localStorage.removeItem("gistId");
-        localStorage.removeItem("gistDate");
         dialog.classList.add("saving");
-        reloadTasks(function() {
+        load(pat, function() {
             dialog.classList.remove("saving");
             dialog.classList.remove("visible");
         });
@@ -126,15 +193,63 @@ function findGist(pat, gistId, callback) {
             console.log(gist);
             var jsonFile = gist.files[getFilename(pat).json];
             var rssFile = gist.files[getFilename(pat).rss];
-            callback(JSON.parse(jsonFile.content), JSON.parse(rssFile.content), new Date(gist.updated_at));
+            callback(JSON.parse(jsonFile.content), new Date(gist.updated_at));
         },
         onError: function(res, code) {
             res = JSON.parse(res);
             console.log("----------------------------------- findGist err");
             console.log(code);
             console.log(res);
+            callback(false);
         }
     });
+}
+
+function generateXML(json) {
+    var xml = '';
+    function add(index, tag) {
+        if (tag.startsWith("itunes:")) var prop = tag.slice(7);
+        else var prop = tag;
+        if (index == "generalInfo") {
+            var value = json.generalInfo[prop];
+        } else {
+            var value = json.items[index][prop];
+        }
+        xml += '<'+tag+'>'+value+'</'+tag+'>';
+    }
+    xml += '<?xml version="1.0" encoding="UTF-8"?>';
+    xml += '<rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">';
+        xml += '<channel>';
+            add("generalInfo", "title");
+            add("generalInfo", "itunes:author");
+            add("generalInfo", "itunes:subtitle");
+            add("generalInfo", "description");
+            add("generalInfo", "link");
+            xml += '<itunes:image href="'+json.generalInfo.imageURL+'"/>';
+            add("generalInfo", "language");
+            xml += '<itunes:category text="'+json.generalInfo.category+'"/>';
+            add("generalInfo", "itunes:explicit");
+            add("generalInfo", "copyright");
+            xml += '<itunes:owner>';
+                add("generalInfo", "itunes:name");
+                add("generalInfo", "itunes:email");
+            xml += '</itunes:owner>';
+
+            for (var i = 0; i > json.items.length; i++) {
+                xml += '<item>';
+                    add(i, "guid");
+                    add(i, "title");
+                    xml += '<enclosure type="audio/mpeg" url="'+json.items[i].audioURL+'" length="0"/>';
+                    xml += '<itunes:image href="'+json.items[i].imageURL+'"/>';
+                    add(i, "itunes:explicit");
+                    add(i, "pubDate");
+                    add(i, "itunes:duration");
+                    add(i, "itunes:summary");
+                xml += '</item>';
+            }
+        xml += '</channel>';
+    xml += '</rss>';
+    return xml;
 }
 
 function createGist(pat, callback) {
@@ -142,8 +257,39 @@ function createGist(pat, callback) {
         description: "RSS Gist created by LaRSS",
         files: {}
     };
+    var defaultJSON = {
+        generalInfo: {
+            title: "",
+            author: "",
+            subtitle: "",
+            description: "",
+            url: "",
+            imageURL: "",
+            explicit: "no",
+            language: "en-us",
+            category: "Music",
+            copyright: "",
+            name: "",
+            email: ""
+        },
+        items: [
+            {
+                guid: "",
+                title: "",
+                audioURL: "",
+                imageURL: "",
+                explicit: "no",
+                date: "Wed, 25 Oct 2017 22:00:00 CET",
+                duration: "",
+                summary: ""
+            }
+        ]
+    };
     req.files[getFilename(pat).json] = {
-        content: JSON.stringify(json)
+        content: JSON.stringify(defaultJSON)
+    };
+    req.files[getFilename(pat).rss] = {
+        content: "empty rn"
     };
     xhr({
         url: "https://api.github.com/gists",
@@ -154,7 +300,7 @@ function createGist(pat, callback) {
             res = JSON.parse(res);
             console.log("----------------------------------- createGist suc");
             console.log(res);
-            callback(res.id, new Date(res.updated_at));
+            callback(res.id, defaultJSON, new Date(res.updated_at));
         },
         onError: function(res, code) {
             res = JSON.parse(res);
@@ -188,6 +334,7 @@ function findGistId(pat, callback) {
             console.log("----------------------------------- findgistid err");
             console.log(code);
             console.log(res);
+            callback(false);
         }
     });
 }
